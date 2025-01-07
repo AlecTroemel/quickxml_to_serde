fdbcca83d50fdd40d8b03202c3f35e556516572c
csko <749306+csko@users.noreply.github.com>
2025-01-07 03:27:01 -0800

    Add NullValue::EmptyString support

--- src/tests.rs
+++ src/tests.rs
@@ -36,6 +36,11 @@ fn test_empty_elements_valid() {
     let expected = json!({ "a": {"b":1} });
     let result = xml_string_to_json(xml.to_owned(), &conf);
     assert_eq!(expected, result.unwrap());
+
+    conf.empty_element_handling = NullValue::EmptyString;
+    let expected = json!({ "a": {"b":1, "x": ""} });
+    let result = xml_string_to_json(xml.to_owned(), &conf);
+    assert_eq!(expected, result.unwrap());
 }
 
 #[test]
@@ -406,11 +411,9 @@ fn test_regex_json_type_overrides() {
         }
     });
 
-    let config = Config::new_with_defaults()
-        .add_json_type_override(
-            Regex::new(r"\w/b").unwrap(),
-            JsonArray::Always(JsonType::Infer
-        )
+    let config = Config::new_with_defaults().add_json_type_override(
+        Regex::new(r"\w/b").unwrap(),
+        JsonArray::Always(JsonType::Infer),
     );
 
     let result = xml_string_to_json(String::from(xml), &config);
@@ -452,13 +455,11 @@ fn test_regex_json_type_overrides() {
         }
     });
 
-    let config = Config::new_with_defaults()
-        .add_json_type_override(
-            Regex::new(r"element").unwrap(),
-            JsonArray::Always(JsonType::Infer)
-        );
+    let config = Config::new_with_defaults().add_json_type_override(
+        Regex::new(r"element").unwrap(),
+        JsonArray::Always(JsonType::Infer),
+    );
 
     let result = xml_string_to_json(String::from(xml), &config);
     assert_eq!(expected, result.unwrap());
-
 }
